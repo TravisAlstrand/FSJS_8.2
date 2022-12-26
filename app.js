c87fa9e83a6b723e1f4b2e03abfb7b3fbdb17938
Travis Alstrand <travisalstrand@gmail.com>
2022-12-26 13:59:43 -0800

    route skeletons / pug files created

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// REQUIRE SEQUELIZE INSTANCE CREATED WHEN SEQUELIZE CLI WAS USED
+const { sequelize } = require('./models/index');
 
+// IMPORT ROUTER
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// VIEW ENGINE SETUP
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,23 +21,41 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// USE ROUTER
+app.use('/books/', indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// CONNECT TO DB / LOG MESSAGE
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to database successful!');
+  })
+  .catch(err => {
+    console.error('Unable to connect to database!', err);
+  });
+
+// SYNC MODELS
+sequelize.sync()
+  .then(() => {
+    console.log('Models synced successfully!');
+  })
+  .catch(err => {
+    console.error('Unable to sync models!', err);
+  });
+
+// CATCH 404 AND FORWARD TO GLOBAL HANDLER
+app.use((req, res, next) => {
+  const err = new Error();
+  err.status = 404;
+  err.message = 'That page does not exist!'
+  next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// GLOBAL ERROR HANDLER
+app.use((err, req, res, next) => {
+  err.status = err.status || 500;
+  err.message = err.message || 'There seems to be a server issue!'
+  console.log(`${err.status}: ${err.message}`);
+  res.render('error', {err});
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+// IMPORT BOOK MODEL
+const books = require('../models/book').Book;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+const asyncHandler = (cb) => {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch (error) {
+      // FORWARD ERROR TO GLOBAL HANDLER
+      next(error);
+    }
+  };
+};
+
+// GET HOME PAGE - ALL BOOKS
+router.get('/', asyncHandler(async (req, res) => {
+  const allBooks = await books.findAll({});
+  console.log(allBooks);
+}));
+
+// GET CREATE NEW BOOK FORM
+router.get('/new', asyncHandler(async (req, res) => {
+
+}));
+
+// POST NEW BOOK
+router.post('/new', asyncHandler(async (req, res) => {
+
+}));
+
+// GET BOOK DETAIL
+router.get('/:id', asyncHandler(async (req, res) => {
+
+}));
+
+// POST BOOK UPDATE
+router.post('/:id', asyncHandler(async (req, res) => {
+
+}));
+
+// POST DELETE BOOK
+router.post('/:id/delete', asyncHandler(async (req, res) => {
+
+}));
 
 module.exports = router;
